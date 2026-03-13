30a953479e006f75b01a779d9d22899ef155f7bd
WurirMacOs <wurir1994@gmail.com>
2026-03-13 23:16:14 +0100

    change with select options

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const h1El = document.querySelector('.input-checked')
+const inputChecked = document.querySelector('.input-checked')
 const inputEl = document.querySelector('input')
+const selectValue = document.querySelector('.select-value')
+const selectEl = document.querySelector('select')
+
+inputChecked.innerText = inputEl.checked
+selectValue.innerText = selectEl.value
 
 inputEl.addEventListener('change', function(e){
     console.log(e.target.checked)
-    h1El.innerText = e.target.checked
+    inputChecked.innerText = e.target.checked
+})
+
+selectEl.addEventListener('change', function(e){
+    selectValue.innerText = e.target.value
 })
