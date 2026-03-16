94245e6abb8d5c4f4738bb526b6bda290c455cbb
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-16 17:13:49 +0100

    onSubmit fn

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const contactForm = document.querySelector('.contact-form')
 const inputEl = document.querySelector('.contact-form__input')
 
-contactForm.addEventListener('submit', function(e){
-    e.preventDefault() // nie przeladowuje strony, zapobiega domyslnemu zachowaniu
+function onSubmit(e){
+    e.preventDefault()
     const value = inputEl.value
     console.log(value);
-    
-})
+}
+
+contactForm.addEventListener('submit', onSubmit)
