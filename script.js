185f50f45afd3975917ab2e6833eac735255f103
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-16 17:58:27 +0100

    square viewport

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 window.addEventListener('scroll', function(e){
     console.log(window.scrollY)
 
+    const navbar = document.querySelector('.navbar')
     const square = document.querySelector('.square')
     const squarePosition = square.getBoundingClientRect()
-    console.log(squarePosition)
+    const squareBottom = squarePosition.bottom
+
+    if(squareBottom < 0){
+        navbar.innerText = 'square is above viewport'
+    } else {
+        navbar.innerText = ''
+    }
 })
