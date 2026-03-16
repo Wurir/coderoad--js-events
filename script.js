111367f960e4bc1087015ad9d11c73701e253e43
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-16 16:26:15 +0100

    event bubbling

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const keydownHeader = document.querySelector('.keydown')
-const keyupHeader = document.querySelector('.keyup')
+const div1 = document.querySelector('.div-1')
+const div2 = document.querySelector('.div-2')
+const div3 = document.querySelector('.div-3')
 
-
-window.addEventListener('keydown', function(e){
-    keydownHeader.innerText = e.key
+div1.addEventListener('click', function(e){
+    console.log('div-1 red');
 })
 
-window.addEventListener('keyup', function(e){
-    keyupHeader.innerText = e.key
+div2.addEventListener('click', function(e){
+    console.log('div-2 yellow');
+})
+
+div3.addEventListener('click', function(e){
+    console.log('div-3 blue');
 })
