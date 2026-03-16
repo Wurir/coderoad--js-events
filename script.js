2f8e2f1f954a01ff9be567332114ac4580004a72
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-16 17:33:40 +0100

    getBoundingClientRect()

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 window.addEventListener('scroll', function(e){
     console.log(window.scrollY)
+
+    const square = document.querySelector('.square')
+    const squarePosition = square.getBoundingClientRect()
+    console.log(squarePosition)
 })
