ec1a8c28a3232d350e8cb0dd57d2177cedee325b
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-18 16:05:21 +0100

    localStorage arrays, objects

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+const namesArr = ['Ala', 'Ola', 'Ela']
+const task = [
+    {
+        isCompleted: true,
+        text: 'Learn Events'
+    }
+]
+
+localStorage.setItem('names', namesArr)
 localStorage.setItem('name', 'Mateusz')
 localStorage.setItem('number', 123)
+localStorage.setItem('task', task)
 
 const name = localStorage.getItem('name')
 const number = Number(localStorage.getItem('number'))
-console.log(name, number)
+const names = localStorage.getItem('names')
+const tasks = localStorage.getItem('task')
+
+
+console.log(name, number, names, tasks)
