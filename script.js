ee2f99163f07c909039cdab6649ff6fc76b1cabc
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-18 15:59:29 +0100

    localStorage strings

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 localStorage.setItem('name', 'Mateusz')
+localStorage.setItem('number', 123)
 
 const name = localStorage.getItem('name')
-console.log(name)
+const number = Number(localStorage.getItem('number'))
+console.log(name, number)
