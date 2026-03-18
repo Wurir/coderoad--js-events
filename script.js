90e1fac334bec7f1a173d670bc3730e705bb32d7
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-18 15:57:37 +0100

    localStorage set/getItem

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-window.addEventListener('scroll', function(e){
-    console.log(window.scrollY)
+localStorage.setItem('name', 'Mateusz')
 
-    const navbar = document.querySelector('.navbar')
-    const square = document.querySelector('.square')
-    const squarePosition = square.getBoundingClientRect()
-    const squareBottom = squarePosition.bottom
-
-    if(squareBottom < 0){
-        navbar.innerText = 'square is above viewport'
-    } else {
-        navbar.innerText = ''
-    }
-})
+const name = localStorage.getItem('name')
+console.log(name)
