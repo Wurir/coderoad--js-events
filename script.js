af290298a9a5bc90abe18ef16e6fc50d62b22e84
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-18 16:37:22 +0100

    sessionStorage

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ const task = [
     }
 ]
 
-localStorage.setItem('names', JSON.stringify(namesArr)) // zapisuje tablice w formie stringa
-localStorage.setItem('name', 'Mateusz')
-localStorage.setItem('number', 123)
-localStorage.setItem('task', JSON.stringify(task))
+const storage = sessionStorage
 
-const name = localStorage.getItem('name')
-const number = Number(localStorage.getItem('number'))
-const names = JSON.parse(localStorage.getItem('names'))
-const tasks = JSON.parse(localStorage.getItem('task'))
+storage.setItem('names', JSON.stringify(namesArr)) // zapisuje tablice w formie stringa
+storage.setItem('name', 'Mateusz')
+storage.setItem('number', 123)
+storage.setItem('task', JSON.stringify(task))
+
+const name = storage.getItem('name')
+const number = Number(storage.getItem('number'))
+const names = JSON.parse(storage.getItem('names'))
+const tasks = JSON.parse(storage.getItem('task'))
 
 
 console.log(name, number, names, tasks)
